6ced657028dccfcf67864ee84d3c906047aa5fd8
FloweyTheFlower420 <guywithanaxe42@gmail.com>
2021-01-08 18:32:42 -0600

    did some more stuff

--- src/main/java/com/hyahm/Main.java
+++ src/main/java/com/hyahm/Main.java
@@ -5,16 +5,17 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = Main.MODID, version = Main.VERSION)
+public class Main
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "hyahm";
     public static final String VERSION = "1.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
 		// some example code
+
         System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
     }
 }
